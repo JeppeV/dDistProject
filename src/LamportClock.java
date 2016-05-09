c1835336db2311e0f40d24bd392c7e1bd80c66da
JeppeV <Jeppevinberg@gmail.com>
2016-05-09 17:17:19 +0200

    attempted implementation of server side log

---
+++ src/LamportClock.java
@@ -0,0 +1,23 @@
+/**
+ * Created by Jeppe Vinberg on 09-05-2016.
+ */
+public class LamportClock {
+
+    private int time;
+
+    public LamportClock(){
+        this.time = 0;
+    }
+
+    public int getTime(){
+        return time;
+    }
+
+    public int generateTimestamp(){
+        return time++;
+    }
+
+    public void processTimestamp(int timestamp){
+        time = Math.max(time, timestamp);
+    }
+}
